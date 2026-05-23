f692ea1123b6496d180c1b55351452cc00fa454b
cbox-2 <msmsas55@gmail.com>
2026-05-23 12:30:29 +0300

    Add CORS support and simplify server log message

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 let messages = [];
@@ -21,5 +23,5 @@ app.post("/messages", (req, res) => {
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log("Server running on port " + PORT);
+  console.log("Server running");
 });
